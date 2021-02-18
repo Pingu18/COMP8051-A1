1bdc658882ee5fc92926789ddcb4243d033806bd
Pingu18 <jaedindhatt@gmail.com>
2021-02-17 17:08:00 -0800

    increment feature

--- test1/ViewController.m
+++ test1/ViewController.m
@@ -10,7 +10,10 @@
 @interface ViewController () {
     Renderer *glesRenderer; // ###
 }
-
+@property (nonatomic, retain) IBOutlet UILabel *posText;
+@property (nonatomic, retain) IBOutlet UILabel *rotText;
+@property (nonatomic, retain) IBOutlet UILabel *val;
+@property (nonatomic, retain) IBOutlet MixTest *theObj;
 @end
 
 @implementation ViewController
@@ -22,7 +25,7 @@
     GLKView *view = (GLKView *)self.view;
     [glesRenderer setup:view];
     [glesRenderer loadModels];
-    NSObject *theObj = [[MixTest alloc] init];
+    _theObj = [[MixTest alloc] init];
     
     UITapGestureRecognizer * tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapResponder:)];
     
@@ -40,35 +43,51 @@
     [button1 addTarget:self action:@selector(button1Pressed:) forControlEvents: UIControlEventTouchUpInside];
     
     UIButton *button2 = [UIButton buttonWithType:UIButtonTypeSystem];
-    [button2 setTitle:@"Increment Count" forState:UIControlStateNormal];
+    [button2 setTitle:@"Toggle Obj-C / C++" forState:UIControlStateNormal];
     [button2 sizeToFit];
-    button2.center = CGPointMake(320/2, 450);
+    button2.center = CGPointMake(320/2 - 80, 450);
     [button2 addTarget:self action:@selector(button2Pressed:) forControlEvents:UIControlEventTouchUpInside];
     
-    UILabel *cubePosTitle = [[UILabel alloc] initWithFrame:CGRectMake(10, 480, 200, 40)];
-    [cubePosTitle setBackgroundColor:[UIColor clearColor]];
+    _posText = [[UILabel alloc] initWithFrame:CGRectMake(10, 480, 200, 40)];
+    [_posText setBackgroundColor:[UIColor clearColor]];
     NSString *posText = [NSString stringWithFormat:@"Position: %.1f, %.1f, %.1f", glesRenderer.posX, glesRenderer.posY, glesRenderer.zoomScale];
-    cubePosTitle.text = posText;
-    [cubePosTitle setTextColor:[UIColor whiteColor]];
+    _posText.text = posText;
+    [_posText setTextColor:[UIColor whiteColor]];
     
-    UILabel *cubeRotTitle = [[UILabel alloc] initWithFrame:CGRectMake(10, 500, 200, 40)];
-    [cubeRotTitle setBackgroundColor:[UIColor clearColor]];
+    _rotText = [[UILabel alloc] initWithFrame:CGRectMake(10, 500, 200, 40)];
+    [_rotText setBackgroundColor:[UIColor clearColor]];
     NSString *rotText = [NSString stringWithFormat:@"Rotation: %.1f, %.1f, %.1f", glesRenderer.rotVecX, glesRenderer.rotVecY * glesRenderer.rotAngle,  glesRenderer.rotVecZ];
-    cubeRotTitle.text = rotText;
-    [cubeRotTitle setTextColor:[UIColor whiteColor]];
+    _rotText.text = rotText;
+    [_rotText setTextColor:[UIColor whiteColor]];
+    
+    _val = [[UILabel alloc] initWithFrame:CGRectMake(320/2 + 40, 431, 200, 40)];
+    [_val setBackgroundColor:[UIColor clearColor]];
+    _val.text = @"Int: ";
+    [_val setTextColor:[UIColor blueColor]];
     
     [self.view addGestureRecognizer:tapRecognizer];
     [self.view addGestureRecognizer:panRecognizer];
     [self.view addGestureRecognizer:pinchRecognizer];
     [self.view addSubview:button1];
     [self.view addSubview:button2];
-    [self.view addSubview:cubePosTitle];
-    [self.view addSubview:cubeRotTitle];
+    [self.view addSubview:_posText];
+    [self.view addSubview:_rotText];
+    [self.view addSubview:_val];
 }
 
 - (void)update
 {
     [glesRenderer update];
+    _posText.text = [NSString stringWithFormat:@"Position: %.1f, %.1f, %.1f", glesRenderer.posX, glesRenderer.posY, glesRenderer.zoomScale];
+    _rotText.text = [NSString stringWithFormat:@"Rotation: %.1f, %.1f, %.1f", glesRenderer.rotVecX * glesRenderer.rotAngle, glesRenderer.rotVecY * glesRenderer.rotAngle, glesRenderer.rotVecZ * glesRenderer.rotAngle];
+    
+    if(_theObj.useObjC)
+    {
+        _val.text = [NSString stringWithFormat:@"Obj-C Int: %.1d", _theObj.val];
+    } else
+    {
+        _val.text = [NSString stringWithFormat:@"C++ Int: %.1d", _theObj.val];
+    }
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
@@ -80,6 +99,10 @@
 {
     //Double tap will start/stop rotation
     glesRenderer.isRotating = !glesRenderer.isRotating;
+    if (glesRenderer.isRotating)
+    {
+        glesRenderer.rotVecY = 1;
+    }
     NSLog(glesRenderer.isRotating ? @"rotating" : @"not rotating");
 }
 
@@ -97,8 +120,6 @@
             glesRenderer.rotVecY = 1.0;
             glesRenderer.rotVecZ = 0.0;
             glesRenderer.rotAngle += 0.001f * velocity.x;
-            if (glesRenderer.rotAngle >= 360.0f)
-                glesRenderer.rotAngle = 0.0f;
         }
         if (fabs(velocity.y) > fabs(velocity.x))
         {
@@ -106,8 +127,6 @@
             glesRenderer.rotVecY = 0.0;
             glesRenderer.rotVecZ = 0.0;
             glesRenderer.rotAngle += 0.001f * velocity.y;
-            if (glesRenderer.rotAngle >= 360.0f)
-                glesRenderer.rotAngle = 0.0f;
         }
         NSLog(@"X Velocity: %.1f, Y Velocity: %.f", velocity.x, velocity.y);
     }
@@ -148,7 +167,9 @@
 }
 
 - (void)button2Pressed: (UIButton *) button2 {
-    NSLog(@"Value: " );
+    _theObj.useObjC = !_theObj.useObjC;
+    _theObj.IncrementValue;
+    NSLog(@"Value: %.1d", _theObj.val);
 }
 
 @end
